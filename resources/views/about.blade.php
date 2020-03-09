403a741855465a8e9dc6a50f14e221fdbf2d2f55
Chhenglin Hor <lin@Lin-Slashs-MacBook-Pro.local>
2020-03-10 00:14:45 +0700

    add other members

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -42,6 +42,83 @@
       </div>
     </div>
   </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/1.jpeg" alt="">
+      <div class="container">
+        <h2>Sok Peav</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Peav!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/2.jpeg" alt="">
+      <div class="container">
+        <h2>Hor Chhenglin</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Chhenglin!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/3.jpeg" alt="">
+      <div class="container">
+        <h2>Rous Sereypanha</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Panha!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/1.jpeg" alt="">
+      <div class="container">
+        <h2>Thay Channarith</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Rith!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/2.jpeg" alt="">
+      <div class="container">
+        <h2>Chou Kuyleng</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Kuyleng!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/3.jpeg" alt="">
+      <div class="container">
+        <h2>Ngoy Mithona</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Mithona!</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-4 col-md-4 col-sm-12 col-12 wow fadeInLeft flex-center nearitem">
+    <div class="card">
+      <img class="card-img-top" src="template/images/3.jpeg" alt="">
+      <div class="container">
+        <h2>Nin Dara Ek</h2>
+        <p class="title">Co-Founder</p>
+        <p>Hi, I'm Dara Ek!</p>
+      </div>
+    </div>
+  </div>
 </div>
 
 @endsection
